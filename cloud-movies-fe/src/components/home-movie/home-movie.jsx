fd938453a75db6634da904bc52d9206c2b11579a
Vladimir Popov <vladapopov02@gmail.com>
2024-04-30 22:59:45 +0200

    feat: add home-movie card component

---
+++ cloud-movies-fe/src/components/home-movie/home-movie.jsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import "./home-movie.css";
+import MovieCover from "@/assets/movie-placeholder.webp";
+import { PlayIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import { Badge } from "@/components/ui/badge";
+
+function HomeMovie() {
+  return (
+    <div className="home-movie" style={{ backgroundImage: `url(${MovieCover})` }}>
+      <div className="movie-details">
+        <Badge className="text-lg px-4 py-1 rounded-full font-medium">Sci-Fi</Badge>
+        <h1>Star Wars</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iure minus rerum et qui ipsam, cum error beatae numquam vero voluptates illum accusamus
+          officia officiis! Distinctio nulla necessitatibus veniam fuga.
+        </p>
+        <div className="details-buttons">
+          <Button variant="outline" className="text-lg py-8 px-10 rounded-full font-medium">
+            <PlayIcon className="mr-2" /> Watch Movie
+          </Button>
+          <Button className="text-lg py-8 px-10 rounded-full font-medium bg-transparent border-2 border-slate-400">
+            More Info <ArrowRightIcon className="ml-2" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeMovie;
