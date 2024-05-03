31c6b87d073163cfa1f9a2313a3a0f096c9fe1bc
Vladimir Popov <vladapopov02@gmail.com>
2024-05-03 15:33:42 +0200

    feat: add navbar links

--- cloud-movies-fe/src/components/navbar/navbar.jsx
+++ cloud-movies-fe/src/components/navbar/navbar.jsx
@@ -1,24 +1,26 @@
 import "./navbar.css";
 import { HamburgerMenuIcon, MagnifyingGlassIcon, BellIcon } from "@radix-ui/react-icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "@/assets/brief-cinema-logo.svg";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import * as React from "react";
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <div className="navbar">
       <div className="navbar-left">
         <div className="white-icon">
           <HamburgerMenuIcon />
         </div>
-        {/* <Link to={""}>Movies</Link>
-        <Link to={""}>New & Popular</Link> */}
-        <a href="">Movies</a>
+        <Link to="/movies">Movies</Link>
         <a href="">New & Popular</a>
       </div>
       <div className="navbar-center">
-        <img src={Logo} alt="logo" />
+        <Link to="/">
+          <img src={Logo} alt="logo" />
+        </Link>
       </div>
       <div className="navbar-right">
         <div className="white-icon">
