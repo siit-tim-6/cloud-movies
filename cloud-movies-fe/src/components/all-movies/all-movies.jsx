7883b20594770e630b5b18287492f0ebc4d059f5
Vladimir Popov <vladapopov02@gmail.com>
2024-05-03 17:13:03 +0200

    feat: add search sidebar

--- cloud-movies-fe/src/components/all-movies/all-movies.jsx
+++ cloud-movies-fe/src/components/all-movies/all-movies.jsx
@@ -1,25 +1,29 @@
 import Navbar from "@/components/navbar/navbar";
 import "./all-movies.css";
 import MovieCard from "./movie-card/movie-card";
+import MovieSearch from "./movie-search/movie-search";
 
 function AllMovies() {
   return (
     <div className="all-movies">
       <Navbar />
-      <div className="all-movies-grid">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+      <div className="all-movies-content">
+        <MovieSearch />
+        <div className="all-movies-grid">
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+        </div>
       </div>
     </div>
   );
