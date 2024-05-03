ba8750462e10ea3a9adf5d74b97a91bcd8c49100
Vladimir Popov <vladapopov02@gmail.com>
2024-05-03 21:44:57 +0200

    feat: change userpool ids

--- cloud-movies-fe/src/aws/UserPool.ts
+++ cloud-movies-fe/src/aws/UserPool.ts
@@ -1,8 +1,8 @@
 import { CognitoUserPool, ICognitoUserPoolData } from "amazon-cognito-identity-js";
 
 const poolData: ICognitoUserPoolData = {
-  UserPoolId: "eu-central-1_FWZCzP40G",
-  ClientId: "2pst9rel5qlqm8tt9bqbleojj5",
+  UserPoolId: "eu-central-1_2I4RSC0Q8",
+  ClientId: "a2e3jp2fnq0000kikvvvhqhpb",
 };
 
 export default new CognitoUserPool(poolData);
