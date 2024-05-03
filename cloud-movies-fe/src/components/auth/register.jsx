7af0b7143a4d6fc5fcf0d54bb4898b135dcc59c4
Vladimir Popov <vladapopov02@gmail.com>
2024-05-03 21:45:44 +0200

    feat: implement register functionality and change input text color

--- cloud-movies-fe/src/components/auth/register.jsx
+++ cloud-movies-fe/src/components/auth/register.jsx
@@ -11,7 +11,8 @@ import { Calendar } from "@/components/ui/calendar";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { format } from "date-fns";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useToast } from "@/components/ui/use-toast";
 
 function Register() {
   const [givenName, setGivenName] = useState("");
@@ -20,6 +21,9 @@ function Register() {
   const [preferredUsername, setPreferredUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const { toast } = useToast();
+  const navigate = useNavigate();
 
   const register = () => {
     const dataGivenName = {
@@ -34,7 +38,7 @@ function Register() {
 
     const dataBirthdate = {
       Name: "birthdate",
-      Value: birthdate,
+      Value: new Date(birthdate).toISOString().split("T")[0],
     };
 
     const dataEmail = {
@@ -54,12 +58,20 @@ function Register() {
       null,
       (err, result) => {
         if (err) {
-          alert(err.message || JSON.stringify(err));
+          toast({
+            title: "Error",
+            description: err.message || JSON.stringify(err),
+          });
           return;
         }
 
         const cognitoUser = result.user;
         console.log(cognitoUser.getUsername());
+        toast({
+          title: "Successful registration",
+          description: "Please check your email and confirm it.",
+        });
+        navigate("/login");
       }
     );
   };
@@ -78,7 +90,7 @@ function Register() {
             </Label>
             <Input
               id="givenName"
-              className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
+              className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
               value={givenName}
               onChange={(event) => setGivenName(event.target.value)}
             />
@@ -87,7 +99,7 @@ function Register() {
             <Label htmlFor="familyName">Last Name</Label>
             <Input
               id="familyName"
-              className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
+              className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
               value={familyName}
               onChange={(event) => setFamilyName(event.target.value)}
             />
@@ -100,7 +112,7 @@ function Register() {
               id="birthdate"
               variant={"outline"}
               className={cn(
-                "w-[240px] justify-start text-left font-normal flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400",
+                "w-[240px] justify-start text-left text-white font-normal flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400",
                 !birthdate && "text-muted-foreground"
               )}
             >
@@ -115,21 +127,21 @@ function Register() {
         <Label htmlFor="email">Email</Label>
         <Input
           id="email"
-          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
+          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
         />
         <Label htmlFor="preferredUsername">Username</Label>
         <Input
           id="preferredUsername"
-          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
+          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
           value={preferredUsername}
           onChange={(event) => setPreferredUsername(event.target.value)}
         />
         <Label htmlFor="password">Password</Label>
         <Input
           id="password"
-          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
+          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
           style={{ marginBottom: 32 }}
           type="password"
           value={password}
@@ -139,6 +151,7 @@ function Register() {
           id="submit-button"
           variant="secondary"
           className="text-lg py-6 px-10 rounded-medium font-bold text-white bg-gradient-to-br from-zinc-900 dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800"
+          onClick={() => register()}
         >
           Register
         </Button>
