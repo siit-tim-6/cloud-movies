dfbbcb2ea23eae77ff940e7ce3edd990912507a7
Vladimir Popov <vladapopov02@gmail.com>
2024-05-03 21:45:26 +0200

    feat: implement login functionality

--- cloud-movies-fe/src/components/auth/login.jsx
+++ cloud-movies-fe/src/components/auth/login.jsx
@@ -3,11 +3,41 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
+import UserPool from "@/aws/UserPool";
+import { useToast } from "@/components/ui/use-toast";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const { toast } = useToast();
+  const navigate = useNavigate();
+
+  const login = () => {
+    const user = new CognitoUser({
+      Username: username,
+      Pool: UserPool,
+    });
+
+    const authDetails = new AuthenticationDetails({
+      Username: username,
+      Password: password,
+    });
+
+    user.authenticateUser(authDetails, {
+      onSuccess: (data) => {
+        navigate("/");
+      },
+      onFailure: (err) => {
+        toast({
+          title: "Unsuccessful login",
+          description: "Wrong credentials or user not confirmed.",
+        });
+      },
+    });
+  };
 
   return (
     <div className="register">
@@ -16,14 +46,14 @@ function Login() {
         <Label htmlFor="username">Username</Label>
         <Input
           id="username"
-          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
+          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
           value={username}
           onChange={(event) => setUsername(event.target.value)}
         />
         <Label htmlFor="password">Password</Label>
         <Input
           id="password"
-          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
+          className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
           style={{ marginBottom: 32 }}
           type="password"
           value={password}
@@ -33,6 +63,7 @@ function Login() {
           id="submit-button"
           variant="secondary"
           className="text-lg py-6 px-10 rounded-medium font-bold text-white bg-gradient-to-br from-zinc-900 dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800"
+          onClick={() => login()}
         >
           Log In
         </Button>
