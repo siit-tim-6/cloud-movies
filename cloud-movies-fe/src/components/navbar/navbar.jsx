803bf431b39ec2c7cdca8b1f6d5ff0bfa23c9977
Vladimir Popov <vladapopov02@gmail.com>
2024-05-05 01:04:14 +0200

    feat: add logout functionality with corresponding ui

--- cloud-movies-fe/src/components/navbar/navbar.jsx
+++ cloud-movies-fe/src/components/navbar/navbar.jsx
@@ -1,12 +1,15 @@
 import "./navbar.css";
-import { HamburgerMenuIcon, MagnifyingGlassIcon, BellIcon } from "@radix-ui/react-icons";
+import { HamburgerMenuIcon, MagnifyingGlassIcon, BellIcon, ExitIcon } from "@radix-ui/react-icons";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "@/assets/brief-cinema-logo.svg";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import * as React from "react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useContext } from "react";
+import { AccountContext } from "@/components/auth/accountContext";
 
 function Navbar() {
   const navigate = useNavigate();
+  const { logout } = useContext(AccountContext);
 
   return (
     <div className="navbar">
@@ -29,9 +32,26 @@ function Navbar() {
         <div className="white-icon">
           <BellIcon />
         </div>
-        <Avatar>
-          <AvatarFallback>VP</AvatarFallback>
-        </Avatar>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar>
+              <AvatarFallback>VP</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              onClick={() => {
+                logout();
+                navigate("/login");
+              }}
+            >
+              <div className="flex flex-row items-center">
+                <ExitIcon className="mr-3" /> Log Out
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
