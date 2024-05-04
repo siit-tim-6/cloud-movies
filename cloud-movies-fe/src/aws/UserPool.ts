44975b89de5a1bba15fd8c54c5616ab7f5d7044a
Vladimir Popov <vladapopov02@gmail.com>
2024-05-05 01:02:03 +0200

    feat: export cognito ids to env file

--- cloud-movies-fe/src/aws/UserPool.ts
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool, ICognitoUserPoolData } from "amazon-cognito-identity-js";
-
-const poolData: ICognitoUserPoolData = {
-  UserPoolId: "eu-central-1_2I4RSC0Q8",
-  ClientId: "a2e3jp2fnq0000kikvvvhqhpb",
-};
-
-export default new CognitoUserPool(poolData);
