27e55e585fbbaf988366b9b3c6d04e19d00812c1
Vladimir Popov <vladapopov02@gmail.com>
2024-05-05 01:04:24 +0200

    feat: setup routes

--- cloud-movies-fe/src/main.jsx
+++ cloud-movies-fe/src/main.jsx
@@ -1,37 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Home from "@/components/home/home.jsx";
 import Login from "@/components/auth/login.jsx";
 import Register from "@/components/auth/register.jsx";
 import AllMovies from "@/components/all-movies/all-movies";
 import { Toaster } from "@/components/ui/toaster";
+import { AccountProvider } from "@/components/auth/accountContext";
+import ProtectedRoutes from "./routes/protectedRoutes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/movies",
-    element: <AllMovies />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<AllMovies />} />
+      </Route>
+    </>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <>
+    <AccountProvider>
       <RouterProvider router={router} />
       <Toaster />
-    </>
+    </AccountProvider>
   </React.StrictMode>
 );
