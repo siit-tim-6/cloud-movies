b476db29ba2f8b8a9d7e93c7d9602e2098ca329d
Teodor Vidakovic <teodorv22@gmail.com>
2024-05-27 11:33:37 +0200

    feat: add link to movie-details page

--- cloud-movies-fe/src/components/all-movies/movie-card/movie-card.jsx
+++ cloud-movies-fe/src/components/all-movies/movie-card/movie-card.jsx
@@ -2,18 +2,21 @@ import "./movie-card.css";
 import { Badge } from "@/components/ui/badge";
 import MovieCover from "@/assets/movie-placeholder.webp";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
+import {Link} from "react-router-dom";
 
 function MovieCard() {
   return (
-    <div className="movie-card">
-      <div className="movie-image" style={{ backgroundImage: `url(${MovieCover})` }}>
-        <Badge>Sci-Fi</Badge>
-      </div>
-      <div className="movie-title">
-        <h3 className="text-white scroll-m-20 text-2xl font-semibold tracking-tight">Star Wars</h3>
-        <ArrowRightIcon className="text-white" />
-      </div>
-    </div>
+      <Link to="/movies/1" className="movie-card-link">
+          <div className="movie-card">
+              <div className="movie-image" style={{ backgroundImage: `url(${MovieCover})` }}>
+                  <Badge>Sci-Fi</Badge>
+              </div>
+              <div className="movie-title">
+                  <h3 className="text-white scroll-m-20 text-2xl font-semibold tracking-tight">Star Wars</h3>
+                  <ArrowRightIcon className="text-white" />
+              </div>
+          </div>
+      </Link>
   );
 }
 
