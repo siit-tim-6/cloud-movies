46fe5b9b5b0279808264c68c7c89fd6cc8ce1c27
Vladimir Popov <vladapopov02@gmail.com>
2024-06-16 00:45:45 +0200

    feat: fetch the list of movies on page load

--- cloud-movies-fe/src/components/all-movies/all-movies.jsx
+++ cloud-movies-fe/src/components/all-movies/all-movies.jsx
@@ -2,27 +2,30 @@ import Navbar from "@/components/navbar/navbar";
 import "./all-movies.css";
 import MovieCard from "./movie-card/movie-card";
 import MovieSearch from "./movie-search/movie-search";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function AllMovies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getAllMovies = async () => {
+      const moviesResponse = await axios.get(`${import.meta.env.VITE_API_URL}/movies`);
+      setMovies(moviesResponse.data);
+    };
+
+    getAllMovies();
+  }, []);
+
   return (
     <div className="all-movies">
       <Navbar />
       <div className="all-movies-content">
         <MovieSearch />
         <div className="all-movies-grid">
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
+          {movies.map((movie) => (
+            <MovieCard Genre={movie.Genre} MovieId={movie.MovieId} Title={movie.Title} key={movie.MovieId} />
+          ))}
         </div>
       </div>
     </div>
