c6595d6925019c2e03b7488369a53e69cf0ae529
Vladimir Popov <vladapopov02@gmail.com>
2024-06-26 01:28:07 +0200

    feat: add cors headers to delete movie lambda response

--- cloud-movies-be/lib/api-gw/src/delete-movie/index.js
+++ cloud-movies-be/lib/api-gw/src/delete-movie/index.js
@@ -9,52 +9,61 @@ const dynamoDocClient = DynamoDBDocumentClient.from(dynamoClient);
 const s3Client = new S3Client({});
 
 exports.handler = async (event) => {
-    const { id } = event.pathParameters;
-    const tableName = process.env.DYNAMODB_TABLE;
-    const bucketName = process.env.S3_BUCKET;
+  const { id } = event.pathParameters;
+  const tableName = process.env.DYNAMODB_TABLE;
+  const bucketName = process.env.S3_BUCKET;
 
-    // Fetch movie details from DynamoDB to get the S3 keys
-    const getParams = {
-        TableName: tableName,
-        Key: { MovieId: id },
-    };
+  // Fetch movie details from DynamoDB to get the S3 keys
+  const getParams = {
+    TableName: tableName,
+    Key: { MovieId: id },
+  };
 
-    const movie = await dynamoDocClient.send(new GetCommand(getParams));
+  const movie = await dynamoDocClient.send(new GetCommand(getParams));
 
-    if (!movie.Item) {
-        return {
-            statusCode: 404,
-            body: JSON.stringify({ message: "Movie not found" }),
-        };
-    }
-
-    const { CoverS3Url, VideoS3Url } = movie.Item;
-
-    // Delete movie from DynamoDB
-    const deleteParams = {
-        TableName: tableName,
-        Key: { MovieId: id },
-    };
-
-    await dynamoDocClient.send(new DeleteCommand(deleteParams));
-
-    // Delete cover and video from S3
-    const coverKey = CoverS3Url.split('/').slice(-3).join('/');
-    const videoKey = VideoS3Url.split('/').slice(-3).join('/');
-
-    await s3Client.send(new DeleteObjectCommand({
-        Bucket: bucketName,
-        Key: coverKey,
-    }));
-
-    await s3Client.send(new DeleteObjectCommand({
-        Bucket: bucketName,
-        Key: videoKey,
-    }));
-
+  if (!movie.Item) {
     return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Movie deleted successfully" }),
+      statusCode: 404,
+      body: JSON.stringify({ message: "Movie not found" }),
     };
-};
+  }
 
+  const { CoverS3Url, VideoS3Url } = movie.Item;
+
+  // Delete movie from DynamoDB
+  const deleteParams = {
+    TableName: tableName,
+    Key: { MovieId: id },
+  };
+
+  await dynamoDocClient.send(new DeleteCommand(deleteParams));
+
+  // Delete cover and video from S3
+  const coverKey = CoverS3Url.split("/").slice(-3).join("/");
+  const videoKey = VideoS3Url.split("/").slice(-3).join("/");
+
+  await s3Client.send(
+    new DeleteObjectCommand({
+      Bucket: bucketName,
+      Key: coverKey,
+    })
+  );
+
+  await s3Client.send(
+    new DeleteObjectCommand({
+      Bucket: bucketName,
+      Key: videoKey,
+    })
+  );
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+      "Access-Control-Allow-Methods": "GET,OPTIONS",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ message: "Movie deleted successfully" }),
+  };
+};
