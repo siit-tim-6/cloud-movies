0d19e3097bd23362c01db90cf1fa623d186f5076
Vladimir Popov <vladapopov02@gmail.com>
2024-06-27 21:03:15 +0200

    feat: add search movies access to data bucket

--- cloud-movies-be/lib/api-gw/api-gw-stack.ts
+++ cloud-movies-be/lib/api-gw/api-gw-stack.ts
@@ -89,6 +89,7 @@ export class ApiGwStack extends cdk.Stack {
       handler: "index.handler",
       code: lambda.Code.fromAsset(path.join(__dirname, "./src/search-movies")),
       environment: {
+        S3_BUCKET: moviesBucket.bucketName,
         DYNAMODB_TABLE: moviesDataTable.tableName,
       },
     });
@@ -106,6 +107,7 @@ export class ApiGwStack extends cdk.Stack {
 
     moviesBucket.grantRead(downloadMovieFn);
     moviesBucket.grantRead(getAllMoviesFn);
+    moviesBucket.grantRead(searchMoviesFn);
     moviesBucket.grantRead(getSingleMovieFn);
     moviesBucket.grantReadWrite(uploadMovieFn);
     moviesBucket.grantReadWrite(deleteMovieFn);
