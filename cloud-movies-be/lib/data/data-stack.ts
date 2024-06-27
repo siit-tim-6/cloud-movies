fa6352e6edce3afa70f9c65db1c7afee756d26f9
Vladimir Popov <vladapopov02@gmail.com>
2024-06-27 23:59:49 +0200

    refactor: use this instead of variables

--- cloud-movies-be/lib/data/data-stack.ts
+++ cloud-movies-be/lib/data/data-stack.ts
@@ -11,14 +11,14 @@ export class DataStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const moviesDataTable = new dynamodb.Table(this, "MoviesData", {
+    this.moviesDataTable = new dynamodb.Table(this, "MoviesData", {
       partitionKey: { name: "MovieId", type: dynamodb.AttributeType.STRING },
       readCapacity: 1,
       writeCapacity: 1,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    moviesDataTable.addGlobalSecondaryIndex({
+    this.moviesDataTable.addGlobalSecondaryIndex({
       indexName: "titleSearch",
       partitionKey: { name: "LowerTitle", type: dynamodb.AttributeType.STRING },
       sortKey: { name: "LowerDescription", type: dynamodb.AttributeType.STRING },
@@ -26,14 +26,14 @@ export class DataStack extends cdk.Stack {
       writeCapacity: 1,
     });
 
-    moviesDataTable.addGlobalSecondaryIndex({
+    this.moviesDataTable.addGlobalSecondaryIndex({
       indexName: "descriptionSearch",
       partitionKey: { name: "LowerDescription", type: dynamodb.AttributeType.STRING },
       readCapacity: 1,
       writeCapacity: 1,
     });
 
-    const subscriptionsDataTable = new dynamodb.Table(this, "SubscriptionsData", {
+    this.subscriptionsDataTable = new dynamodb.Table(this, "SubscriptionsData", {
       partitionKey: { name: "UserId", type: dynamodb.AttributeType.STRING },
       sortKey: { name: "SubscribedTo", type: dynamodb.AttributeType.STRING },
       readCapacity: 1,
@@ -41,14 +41,14 @@ export class DataStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    subscriptionsDataTable.addGlobalSecondaryIndex({
+    this.subscriptionsDataTable.addGlobalSecondaryIndex({
       indexName: "SubscribedToSearch",
       partitionKey: { name: "SubscribedTo", type: dynamodb.AttributeType.STRING },
       readCapacity: 1,
       writeCapacity: 1,
     });
 
-    const moviesBucket = new s3.Bucket(this, "MoviesBucket", {
+    this.moviesBucket = new s3.Bucket(this, "MoviesBucket", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       cors: [
@@ -59,9 +59,5 @@ export class DataStack extends cdk.Stack {
         },
       ],
     });
-
-    this.moviesDataTable = moviesDataTable;
-    this.subscriptionsDataTable = subscriptionsDataTable;
-    this.moviesBucket = moviesBucket;
   }
 }
