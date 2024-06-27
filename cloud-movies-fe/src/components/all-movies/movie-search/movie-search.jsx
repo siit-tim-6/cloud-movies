8a0eea9da1fd3e4f015f37d74caec49d2836e4f1
Vladimir Popov <vladapopov02@gmail.com>
2024-06-27 20:01:18 +0200

    refactor: make all search nouns singular

--- cloud-movies-fe/src/components/all-movies/movie-search/movie-search.jsx
+++ cloud-movies-fe/src/components/all-movies/movie-search/movie-search.jsx
@@ -7,12 +7,12 @@ import { useState } from "react";
 function MovieSearch({ setMovies }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [actors, setActors] = useState("");
-  const [directors, setDirectors] = useState("");
-  const [genres, setGenres] = useState("");
+  const [actor, setActor] = useState("");
+  const [director, setDirector] = useState("");
+  const [genre, setGenre] = useState("");
 
   const handleSearch = async () => {
-    let query = `title=${title}&description=${description}&actors=${actors}&directors=${directors}&genres=${genres}`;
+    let query = `title=${title}&description=${description}&actor=${actor}&director=${director}&genre=${genre}`;
     const response = await fetch(`${import.meta.env.VITE_API_URL}/search-movies?${query}`, {
       method: "GET",
     });
@@ -46,22 +46,22 @@ function MovieSearch({ setMovies }) {
       <Input
         id="actors"
         className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
-        value={actors}
-        onChange={(event) => setActors(event.target.value)}
+        value={actor}
+        onChange={(event) => setActor(event.target.value)}
       />
       <Label htmlFor="directors">Directors</Label>
       <Input
         id="directors"
         className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
-        value={directors}
-        onChange={(event) => setDirectors(event.target.value)}
+        value={director}
+        onChange={(event) => setDirector(event.target.value)}
       />
       <Label htmlFor="genres">Genres</Label>
       <Input
         id="genres"
         className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-white dark:text-white rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
-        value={genres}
-        onChange={(event) => setGenres(event.target.value)}
+        value={genre}
+        onChange={(event) => setGenre(event.target.value)}
       />
       <Button
         id="submit-button"
