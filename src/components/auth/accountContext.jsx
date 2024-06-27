49164bc1433c1dbf27dab923d81b00f0efc16465
Vladimir Popov <vladapopov02@gmail.com>
2024-06-28 01:56:19 +0200

    feat: add a method for fetching user role on fronted

--- src/components/auth/accountContext.jsx
+++ src/components/auth/accountContext.jsx
@@ -54,7 +54,30 @@ const AccountProvider = (props) => {
     }
   };
 
-  return <AccountContext.Provider value={{ authenticate, getSession, logout }}>{props.children}</AccountContext.Provider>;
+  const getRole = async () => {
+    const session = await getSession();
+    const jwtToken = session.accessToken.jwtToken;
+
+    const base64Url = jwtToken.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+
+    const jsonData = JSON.parse(jsonPayload);
+
+    if (jsonData["cognito:groups"].includes("Admins")) return "ADMIN";
+    else if (jsonData["cognito:groups"].includes("RegularUsers")) return "USER";
+    return undefined;
+  };
+
+  return <AccountContext.Provider value={{ authenticate, getSession, logout, getRole }}>{props.children}</AccountContext.Provider>;
 };
 
 export { AccountProvider, AccountContext };
