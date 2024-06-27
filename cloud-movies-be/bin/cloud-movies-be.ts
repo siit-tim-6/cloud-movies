0cca863c8ffaeddb5f93ab7830789816e7404dfa
Vladimir Popov <vladapopov02@gmail.com>
2024-06-28 00:04:08 +0200

    feat: connect exported variables

--- cloud-movies-be/bin/cloud-movies-be.ts
+++ cloud-movies-be/bin/cloud-movies-be.ts
@@ -10,13 +10,26 @@ import { LambdaStack } from "../lib/lambda/lambda-stack";
 const app = new cdk.App();
 
 new CognitoStack(app, "CognitoStack", {});
+
 const dataStack = new DataStack(app, "DataStack", {});
+
 const lambdaStack = new LambdaStack(app, "LambdaStack", {
   moviesBucket: dataStack.moviesBucket,
   moviesDataTable: dataStack.moviesDataTable,
   subscriptionsDataTable: dataStack.subscriptionsDataTable,
 });
-new ApiGwStack(app, "ApiGwStack", {});
+
+new ApiGwStack(app, "ApiGwStack", {
+  uploadMovieFn: lambdaStack.uploadMovieFn,
+  downloadMovieFn: lambdaStack.downloadMovieFn,
+  getMoviesFn: lambdaStack.getMoviesFn,
+  deleteMovieFn: lambdaStack.deleteMovieFn,
+  getSingleMovieFn: lambdaStack.getSingleMovieFn,
+  getSubscriptionsFn: lambdaStack.getSubscriptionsFn,
+  subscribeFn: lambdaStack.subscribeFn,
+  unsubscribeFn: lambdaStack.unsubscribeFn,
+});
+
 new FrontDeploymentStack(app, "FrontDeploymentStack", {});
 
 app.synth();
