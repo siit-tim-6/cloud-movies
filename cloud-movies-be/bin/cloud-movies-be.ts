aa97a83ea04f108bc5fefe327113b7e49287b66e
Vladimir Popov <vladapopov02@gmail.com>
2024-06-27 23:22:44 +0200

    feat: define multistack app

--- cloud-movies-be/bin/cloud-movies-be.ts
+++ cloud-movies-be/bin/cloud-movies-be.ts
@@ -4,10 +4,14 @@ import * as cdk from "aws-cdk-lib";
 import { CognitoStack } from "../lib/cognito/cognito-stack";
 import { ApiGwStack } from "../lib/api-gw/api-gw-stack";
 import { FrontDeploymentStack } from "../lib/front-deployment/front-deployment-stack";
+import { DataStack } from "../lib/data/data-stack";
+import { LambdaStack } from "../lib/lambda/lambda-stack";
 
 const app = new cdk.App();
 
 new CognitoStack(app, "CognitoStack", {});
+new DataStack(app, "DataStack", {});
+new LambdaStack(app, "LambdaStack", {});
 new ApiGwStack(app, "ApiGwStack", {});
 new FrontDeploymentStack(app, "FrontDeploymentStack", {});
 
