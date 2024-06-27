df0e3689fe013ca070d44afb68bffb2d6ab2f424
Vladimir Popov <vladapopov02@gmail.com>
2024-06-27 23:22:25 +0200

    feat: api-gw stack now only has the api gateway logic

--- cloud-movies-be/lib/api-gw/api-gw-stack.ts
+++ cloud-movies-be/lib/api-gw/api-gw-stack.ts
@@ -1,111 +1,29 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import * as s3 from "aws-cdk-lib/aws-s3";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import path = require("path");
 
 export class ApiGwStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const moviesDataTable = new dynamodb.Table(this, "MoviesData", {
-      partitionKey: { name: "MovieId", type: dynamodb.AttributeType.STRING },
-      readCapacity: 1,
-      writeCapacity: 1,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
+    const uploadMovieFn = lambda.Function.fromFunctionArn(this, "uploadMovieFn", cdk.Fn.importValue("uploadMovieFnArn"));
+    const downloadMovieFn = lambda.Function.fromFunctionArn(this, "downloadMovieFn", cdk.Fn.importValue("downloadMovieFnArn"));
+    const getSingleMovieFn = lambda.Function.fromFunctionArn(this, "getSingleMovieFn", cdk.Fn.importValue("getSingleMovieFnArn"));
+    const getMoviesFn = lambda.Function.fromFunctionArn(this, "getMoviesFn", cdk.Fn.importValue("getMoviesFnArn"));
+    const deleteMovieFn = lambda.Function.fromFunctionArn(this, "deleteMovieFn", cdk.Fn.importValue("deleteMovieFnArn"));
+    const subscribeFn = lambda.Function.fromFunctionArn(this, "subscribeFn", cdk.Fn.importValue("subscribeFnArn"));
+    const getSubscriptionsFn = lambda.Function.fromFunctionArn(this, "getSubscriptionsFn", cdk.Fn.importValue("getSubscriptionsFnArn"));
+    const unsubscribeFn = lambda.Function.fromFunctionArn(this, "unsubscribeFn", cdk.Fn.importValue("unsubscribeFnArn"));
 
-    moviesDataTable.addGlobalSecondaryIndex({
-      indexName: "titleSearch",
-      partitionKey: { name: "LowerTitle", type: dynamodb.AttributeType.STRING },
-      sortKey: { name: "LowerDescription", type: dynamodb.AttributeType.STRING },
-      readCapacity: 1,
-      writeCapacity: 1,
-    });
-
-    moviesDataTable.addGlobalSecondaryIndex({
-      indexName: "descriptionSearch",
-      partitionKey: { name: "LowerDescription", type: dynamodb.AttributeType.STRING },
-      readCapacity: 1,
-      writeCapacity: 1,
-    });
-
-    const moviesBucket = new s3.Bucket(this, "MoviesBucket", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      cors: [
-        {
-          allowedOrigins: ["*"],
-          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT, s3.HttpMethods.POST, s3.HttpMethods.DELETE],
-          allowedHeaders: ["*"],
-        },
-      ],
-    });
-
-    const uploadMovieFn = new lambda.Function(this, "uploadMovieFn", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "./src/upload-movie")),
-      environment: {
-        S3_BUCKET: moviesBucket.bucketName,
-        DYNAMODB_TABLE: moviesDataTable.tableName,
-      },
-    });
-
-    const downloadMovieFn = new lambda.Function(this, "downloadMovieFn", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "./src/download-movie")),
-      environment: {
-        S3_BUCKET: moviesBucket.bucketName,
-        DYNAMODB_TABLE: moviesDataTable.tableName,
-      },
-    });
-
-    const getSingleMovieFn = new lambda.Function(this, "getSingleMovieFn", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "./src/get-single-movie")),
-      environment: {
-        S3_BUCKET: moviesBucket.bucketName,
-        DYNAMODB_TABLE: moviesDataTable.tableName,
-      },
-    });
-
-    const getMoviesFn = new lambda.Function(this, "getMoviesFn", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "./src/get-movies")),
-      environment: {
-        S3_BUCKET: moviesBucket.bucketName,
-        DYNAMODB_TABLE: moviesDataTable.tableName,
-      },
-    });
-
-    const deleteMovieFn = new lambda.Function(this, "deleteMovieFn", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "./src/delete-movie")),
-      environment: {
-        S3_BUCKET: moviesBucket.bucketName,
-        DYNAMODB_TABLE: moviesDataTable.tableName,
-      },
-      timeout: cdk.Duration.seconds(10),
-    });
-
-    moviesBucket.grantRead(downloadMovieFn);
-    moviesBucket.grantRead(getMoviesFn);
-    moviesBucket.grantRead(getSingleMovieFn);
-    moviesBucket.grantReadWrite(uploadMovieFn);
-    moviesBucket.grantReadWrite(deleteMovieFn);
-
-    moviesDataTable.grantReadData(downloadMovieFn);
-    moviesDataTable.grantReadData(getSingleMovieFn);
-    moviesDataTable.grantReadData(getMoviesFn);
-    moviesDataTable.grantReadWriteData(uploadMovieFn);
-    moviesDataTable.grantReadWriteData(deleteMovieFn);
+    const uploadMovieLambdaIntegration = new apigateway.LambdaIntegration(uploadMovieFn);
+    const downloadMovieLambdaIntegration = new apigateway.LambdaIntegration(downloadMovieFn);
+    const getSingleMovieLambdaIntegration = new apigateway.LambdaIntegration(getSingleMovieFn);
+    const getMoviesLambdaIntegration = new apigateway.LambdaIntegration(getMoviesFn);
+    const deleteMovieLambdaIntegration = new apigateway.LambdaIntegration(deleteMovieFn);
+    const subscribeLambdaIntegration = new apigateway.LambdaIntegration(subscribeFn);
+    const getSubscriptionsLambdaIntegration = new apigateway.LambdaIntegration(getSubscriptionsFn);
+    const unsubscribeLambdaIntegration = new apigateway.LambdaIntegration(unsubscribeFn);
 
     const api = new apigateway.RestApi(this, "MoviesApi", {
       restApiName: "Movies Service",
@@ -133,7 +51,6 @@ export class ApiGwStack extends cdk.Stack {
       },
     });
 
-    const uploadMovieLambdaIntegration = new apigateway.LambdaIntegration(uploadMovieFn);
     const uploadMovieResource = api.root.addResource("upload-movie");
     uploadMovieResource.addMethod("POST", uploadMovieLambdaIntegration, {
       requestModels: {
@@ -147,7 +64,6 @@ export class ApiGwStack extends cdk.Stack {
       allowOrigins: ["*"],
     });
 
-    const downloadMovieLambdaIntegration = new apigateway.LambdaIntegration(downloadMovieFn);
     const downloadMovieResource = api.root.addResource("download-movie");
     downloadMovieResource.addMethod("GET", downloadMovieLambdaIntegration, {
       requestParameters: {
@@ -159,19 +75,6 @@ export class ApiGwStack extends cdk.Stack {
     });
 
     const moviesResource = api.root.addResource("movies");
-
-    const movieResource = moviesResource.addResource("{id}");
-    const getSingleMovieLambdaIntegration = new apigateway.LambdaIntegration(getSingleMovieFn);
-    movieResource.addMethod("GET", getSingleMovieLambdaIntegration, {
-      requestParameters: {
-        "method.request.path.id": true,
-      },
-      requestValidatorOptions: {
-        validateRequestParameters: true,
-      },
-    });
-
-    const getMoviesLambdaIntegration = new apigateway.LambdaIntegration(getMoviesFn);
     moviesResource.addMethod("GET", getMoviesLambdaIntegration, {
       requestParameters: {
         "method.request.path.title": false,
@@ -185,7 +88,15 @@ export class ApiGwStack extends cdk.Stack {
       },
     });
 
-    const deleteMovieLambdaIntegration = new apigateway.LambdaIntegration(deleteMovieFn);
+    const movieResource = moviesResource.addResource("{id}");
+    movieResource.addMethod("GET", getSingleMovieLambdaIntegration, {
+      requestParameters: {
+        "method.request.path.id": true,
+      },
+      requestValidatorOptions: {
+        validateRequestParameters: true,
+      },
+    });
     movieResource.addMethod("DELETE", deleteMovieLambdaIntegration, {
       requestParameters: {
         "method.request.path.id": true,
@@ -196,49 +107,6 @@ export class ApiGwStack extends cdk.Stack {
     });
     movieResource.addCorsPreflight({
       allowOrigins: ["*"],
-    });
-
-    // subscription related things
-    const subscriptionsDataTable = new dynamodb.Table(this, "SubscriptionsData", {
-      partitionKey: { name: "UserId", type: dynamodb.AttributeType.STRING },
-      sortKey: { name: "SubscribedTo", type: dynamodb.AttributeType.STRING },
-      readCapacity: 1,
-      writeCapacity: 1,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-
-    subscriptionsDataTable.addGlobalSecondaryIndex({
-      indexName: "SubscribedToSearch",
-      partitionKey: { name: "SubscribedTo", type: dynamodb.AttributeType.STRING },
-      readCapacity: 1,
-      writeCapacity: 1,
-    });
-
-    const subscribeFn = new lambda.Function(this, "subscribeFn", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "./src/subscribe")),
-      environment: {
-        DYNAMODB_TABLE: subscriptionsDataTable.tableName,
-      },
-    });
-
-    const getSubscriptionsFn = new lambda.Function(this, "getSubscriptionsFn", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "./src/get-subscriptions")),
-      environment: {
-        DYNAMODB_TABLE: subscriptionsDataTable.tableName,
-      },
-    });
-
-    const unsubscibeFn = new lambda.Function(this, "unsubscribeFn", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "./src/unsubscribe")),
-      environment: {
-        DYNAMODB_TABLE: subscriptionsDataTable.tableName,
-      },
     });
 
     const subscribeRequestBodySchema = new apigateway.Model(this, "subscribeRequestBodySchema", {
@@ -253,7 +121,6 @@ export class ApiGwStack extends cdk.Stack {
       },
     });
 
-    const subscribeLambdaIntegration = new apigateway.LambdaIntegration(subscribeFn);
     const subscriptionsResource = api.root.addResource("subscriptions");
     subscriptionsResource.addMethod("POST", subscribeLambdaIntegration, {
       requestModels: {
@@ -266,11 +133,7 @@ export class ApiGwStack extends cdk.Stack {
     subscriptionsResource.addCorsPreflight({
       allowOrigins: ["*"],
     });
-
-    const getSubscriptionsLambdaIntegration = new apigateway.LambdaIntegration(getSubscriptionsFn);
     subscriptionsResource.addMethod("GET", getSubscriptionsLambdaIntegration);
-
-    const unsubscribeLambdaIntegration = new apigateway.LambdaIntegration(unsubscibeFn);
     subscriptionsResource.addMethod("DELETE", unsubscribeLambdaIntegration, {
       requestParameters: {
         "method.request.querystring.subscribedTo": true,
@@ -279,9 +142,5 @@ export class ApiGwStack extends cdk.Stack {
         validateRequestParameters: true,
       },
     });
-
-    subscriptionsDataTable.grantWriteData(subscribeFn);
-    subscriptionsDataTable.grantReadData(getSubscriptionsFn);
-    subscriptionsDataTable.grantWriteData(unsubscibeFn);
   }
 }
