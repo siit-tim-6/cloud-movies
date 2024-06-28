324f6526a4269b4c3bc6faa3a40e584148239211
Teodor Vidakovic <teodorv22@gmail.com>
2024-06-28 09:25:13 +0200

    feat: adjust upload page for editing as well

--- src/components/upload-movie/upload-movie.jsx
+++ src/components/upload-movie/upload-movie.jsx
@@ -1,218 +1,270 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "@/components/navbar/navbar.jsx";
 import "./upload-movie.css";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { AccountContext } from "../auth/accountContext";
 import { Button } from "../ui/button";
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
-function UploadMovie() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+function UploadMovie({ isEditMode = false }) {
+  const [movieDetails, setMovieDetails] = useState({
+    title: "",
+    description: "",
+    genres: [""],
+    actors: [""],
+    directors: [""],
+    coverFileName: "",
+    coverFileType: "",
+    videoFileName: "",
+    videoFileType: "",
+    coverS3Url: "",
+    videoS3Url: ""
+  });
+
   const [cover, setCover] = useState(null);
   const [video, setVideo] = useState(null);
-  const [genreInputElems, setGenreInputElems] = useState([""]);
-  const [actorInputElems, setActorInputElems] = useState([""]);
-  const [directorInputElems, setDirectorInputElems] = useState([""]);
 
   const navigate = useNavigate();
   const { getSession } = useContext(AccountContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (isEditMode) {
+      fetchMovieDetails().then(() => {
+        console.log("State after fetching movie details:", movieDetails);
+      });
+    }
+  }, [isEditMode, id]);
+
+  const fetchMovieDetails = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/movies/${id}`);
+      const movie = response.data;
+      console.log("Fetched movie details:", movie);
+
+      const coverFileName = movie.CoverS3Url.split('/').pop();
+      const videoFileName = movie.VideoS3Url.split('/').pop();
+      const coverFileType = inferFileType(coverFileName);
+      const videoFileType = inferFileType(videoFileName);
+
+      setMovieDetails({
+        title: movie.Title || "",
+        description: movie.Description || "",
+        genres: movie.Genres || [""],
+        actors: movie.Actors || [""],
+        directors: movie.Directors || [""],
+        coverFileName: coverFileName || "",
+        coverFileType: coverFileType || "",
+        videoFileName: videoFileName || "",
+        videoFileType: videoFileType || "",
+        coverS3Url: movie.CoverS3Url || "",
+        videoS3Url: movie.VideoS3Url || ""
+      });
+    } catch (error) {
+      console.error("Error fetching movie details:", error);
+    }
+  };
+
+  const inferFileType = (fileName) => {
+    const extension = fileName.split('.').pop();
+    switch (extension) {
+      case 'jpg':
+      case 'jpeg':
+        return 'image/jpeg';
+      case 'png':
+        return 'image/png';
+      case 'mp4':
+        return 'video/mp4';
+      default:
+        return '';
+    }
+  };
 
   const handleAddInput = (type) => {
-    switch (type) {
-      case "genre":
-        setGenreInputElems([...genreInputElems, ""]);
-        break;
-      case "actor":
-        setActorInputElems([...actorInputElems, ""]);
-        break;
-      case "director":
-        setDirectorInputElems([...directorInputElems, ""]);
-        break;
-    }
+    setMovieDetails((prevDetails) => ({
+      ...prevDetails,
+      [type]: [...prevDetails[type], ""],
+    }));
   };
 
   const handleRemoveInput = (type, index) => {
-    switch (type) {
-      case "genre":
-        const inputElCopy1 = [...genreInputElems];
-        inputElCopy1.splice(index, 1);
-        setGenreInputElems(inputElCopy1);
-        break;
-      case "actor":
-        const inputElCopy2 = [...actorInputElems];
-        inputElCopy2.splice(index, 1);
-        setActorInputElems(inputElCopy2);
-        break;
-      case "director":
-        const inputElCopy3 = [...directorInputElems];
-        inputElCopy3.splice(index, 1);
-        setDirectorInputElems(inputElCopy3);
-        break;
-    }
+    setMovieDetails((prevDetails) => ({
+      ...prevDetails,
+      [type]: prevDetails[type].filter((_, i) => i !== index),
+    }));
   };
 
   const handleChangeInput = (type, index, newVal) => {
-    switch (type) {
-      case "genre":
-        const inputElCopy1 = [...genreInputElems];
-        inputElCopy1[index] = newVal;
-        setGenreInputElems(inputElCopy1);
-        break;
-      case "actor":
-        const inputElCopy2 = [...actorInputElems];
-        inputElCopy2[index] = newVal;
-        setActorInputElems(inputElCopy2);
-        break;
-      case "director":
-        const inputElCopy3 = [...directorInputElems];
-        inputElCopy3[index] = newVal;
-        setDirectorInputElems(inputElCopy3);
-        break;
-    }
+    setMovieDetails((prevDetails) => ({
+      ...prevDetails,
+      [type]: prevDetails[type].map((val, i) => (i === index ? newVal : val)),
+    }));
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const session = await getSession();
+    let apiUrl = ""
+
+    if (isEditMode) {
+      apiUrl = `${import.meta.env.VITE_API_URL}/movies/${id}`;
+    } else {
+      apiUrl = `${import.meta.env.VITE_API_URL}/movies`;
+    }
 
     try {
-      const metadataResponse = await axios.post(
-        `${import.meta.env.VITE_API_URL}/movies`,
-        {
-          title,
-          description,
-          genres: genreInputElems,
-          actors: actorInputElems,
-          directors: directorInputElems,
-          coverFileName: cover.name,
-          coverFileType: cover.type,
-          videoFileName: video.name,
-          videoFileType: video.type,
+      const metadataResponse = await axios({
+        method: isEditMode ? "put" : "post",
+        url: apiUrl,
+        data: {
+          title: movieDetails.title,
+          description: movieDetails.description,
+          genres: movieDetails.genres,
+          actors: movieDetails.actors,
+          directors: movieDetails.directors,
+          coverFileName: cover ? cover.name : movieDetails.coverFileName,
+          coverFileType: cover ? cover.type : movieDetails.coverFileType,
+          videoFileName: video ? video.name : movieDetails.videoFileName,
+          videoFileType: video ? video.type : movieDetails.videoFileType,
         },
-        {
-          headers: {
-            Authorization: session.accessToken.jwtToken,
-          },
-        }
-      );
+        headers: {
+          Authorization: session.accessToken.jwtToken,
+        },
+      });
 
       const { coverUploadURL, videoUploadURL } = metadataResponse.data;
-      console.log(coverUploadURL, videoUploadURL);
 
-      // Upload the cover image to S3
-      await axios.put(coverUploadURL, cover, {
-        headers: {
-          "Content-Type": cover.type,
-        },
-      });
+      if (cover && coverUploadURL) {
+        await axios.put(coverUploadURL, cover, {
+          headers: {
+            "Content-Type": cover.type,
+          },
+        });
+      }
 
-      // Upload the video to S3
-      await axios.put(videoUploadURL, video, {
-        headers: {
-          "Content-Type": video.type,
-        },
-      });
+      if (video && videoUploadURL) {
+        await axios.put(videoUploadURL, video, {
+          headers: {
+            "Content-Type": video.type,
+          },
+        });
+      }
 
-      alert("Movie uploaded successfully!");
+      alert(`Movie ${isEditMode ? "updated" : "uploaded"} successfully!`);
       navigate("/movies");
     } catch (error) {
-      console.error("Error uploading movie:", error);
-      alert("Failed to upload movie.");
+      console.error(`Error ${isEditMode ? "updating" : "uploading"} movie:`, error);
+      alert(`Failed to ${isEditMode ? "update" : "upload"} movie.`);
     }
   };
 
   return (
-    <>
-      <Navbar />
-      <div className="upload-movie-page">
-        <h1 className="title">Upload Movie</h1>
-        <form className="upload-form" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} required></textarea>
-          </div>
-          <div className="form-group">
-            <label htmlFor="genre-0">Genre</label>
-            {genreInputElems.map((_, i) => (
-              <div key={`genre${i}`} className="input-line">
-                <input
-                  type="text"
-                  id={`genre-${i}`}
-                  value={genreInputElems[i]}
-                  onChange={(e) => {
-                    handleChangeInput("genre", i, e.target.value);
-                  }}
-                  required
-                />
-                <Button type="button">
-                  {i !== genreInputElems.length - 1 ? (
-                    <MinusIcon onClick={(e) => handleRemoveInput("genre", i)} />
-                  ) : (
-                    <PlusIcon onClick={() => handleAddInput("genre")} />
-                  )}
-                </Button>
-              </div>
-            ))}
-          </div>
-          <div className="form-group">
-            <label htmlFor="actor-0">Actors</label>
-            {actorInputElems.map((_, i) => (
-              <div key={`actor${i}`} className="input-line">
-                <input type="text" id={`actor-${i}`} value={actorInputElems[i]} onChange={(e) => handleChangeInput("actor", i, e.target.value)} required />
-                <Button type="button">
-                  {i !== actorInputElems.length - 1 ? (
-                    <MinusIcon onClick={(e) => handleRemoveInput("actor", i)} />
-                  ) : (
-                    <PlusIcon onClick={() => handleAddInput("actor")} />
-                  )}
-                </Button>
-              </div>
-            ))}
-          </div>
-          <div className="form-group">
-            <label htmlFor="director-0">Directors</label>
-            {directorInputElems.map((_, i) => (
-              <div key={`director${i}`} className="input-line">
-                <input type="text" id={`director-${i}`} value={directorInputElems[i]} onChange={(e) => handleChangeInput("director", i, e.target.value)} required />
-                <Button type="button">
-                  {i !== directorInputElems.length - 1 ? (
-                    <MinusIcon onClick={(e) => handleRemoveInput("director", i)} />
-                  ) : (
-                    <PlusIcon onClick={() => handleAddInput("director")} />
-                  )}
-                </Button>
-              </div>
-            ))}
-          </div>
-          <div className="form-group">
-            <label htmlFor="cover">Movie Cover</label>
-            <div className="custom-file-input">
-              <input type="file" id="cover" accept="image/*" onChange={(e) => setCover(e.target.files[0])} required />
-              <span>Choose Cover Image</span>
+      <>
+        <Navbar />
+        <div className="upload-movie-page">
+          <h1 className="title">{isEditMode ? "Edit" : "Upload"} Movie</h1>
+          <form className="upload-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" value={movieDetails.title} onChange={(e) => setMovieDetails({ ...movieDetails, title: e.target.value })} required />
             </div>
-            {cover && <img src={URL.createObjectURL(cover)} alt="Movie Cover" className="cover-preview" />}
-          </div>
-          <div className="form-group">
-            <label htmlFor="video">Movie File (MP4)</label>
-            <div className="custom-file-input">
-              <input type="file" id="video" accept="video/mp4" onChange={(e) => setVideo(e.target.files[0])} required />
-              <span>Choose MP4 File</span>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea id="description" value={movieDetails.description} onChange={(e) => setMovieDetails({ ...movieDetails, description: e.target.value })} required></textarea>
             </div>
-            {video && <video src={URL.createObjectURL(video)} controls className="video-preview"></video>}
-          </div>
-          <button type="submit" className="submit-button">
-            Upload Movie
-          </button>
-        </form>
-      </div>
-    </>
+            <div className="form-group">
+              <label htmlFor="genre-0">Genre</label>
+              {movieDetails.genres.map((genre, i) => (
+                  <div key={`genre${i}`} className="input-line">
+                    <input
+                        type="text"
+                        id={`genre-${i}`}
+                        value={genre}
+                        onChange={(e) => handleChangeInput("genres", i, e.target.value)}
+                        required
+                    />
+                    <Button type="button">
+                      {i !== movieDetails.genres.length - 1 ? (
+                          <MinusIcon onClick={() => handleRemoveInput("genres", i)} />
+                      ) : (
+                          <PlusIcon onClick={() => handleAddInput("genres")} />
+                      )}
+                    </Button>
+                  </div>
+              ))}
+            </div>
+            <div className="form-group">
+              <label htmlFor="actor-0">Actors</label>
+              {movieDetails.actors.map((actor, i) => (
+                  <div key={`actor${i}`} className="input-line">
+                    <input
+                        type="text"
+                        id={`actor-${i}`}
+                        value={actor}
+                        onChange={(e) => handleChangeInput("actors", i, e.target.value)}
+                        required
+                    />
+                    <Button type="button">
+                      {i !== movieDetails.actors.length - 1 ? (
+                          <MinusIcon onClick={() => handleRemoveInput("actors", i)} />
+                      ) : (
+                          <PlusIcon onClick={() => handleAddInput("actors")} />
+                      )}
+                    </Button>
+                  </div>
+              ))}
+            </div>
+            <div className="form-group">
+              <label htmlFor="director-0">Directors</label>
+              {movieDetails.directors.map((director, i) => (
+                  <div key={`director${i}`} className="input-line">
+                    <input
+                        type="text"
+                        id={`director-${i}`}
+                        value={director}
+                        onChange={(e) => handleChangeInput("directors", i, e.target.value)}
+                        required
+                    />
+                    <Button type="button">
+                      {i !== movieDetails.directors.length - 1 ? (
+                          <MinusIcon onClick={() => handleRemoveInput("directors", i)} />
+                      ) : (
+                          <PlusIcon onClick={() => handleAddInput("directors")} />
+                      )}
+                    </Button>
+                  </div>
+              ))}
+            </div>
+            <div className="form-group">
+              <label htmlFor="cover">Movie Cover</label>
+              <div className="custom-file-input">
+                <input type="file" id="cover" accept="image/*" onChange={(e) => setCover(e.target.files[0])} />
+                <span>Choose Cover Image</span>
+              </div>
+              {movieDetails.coverS3Url && !cover && (
+                  <img src={movieDetails.coverS3Url} alt="Movie Cover" className="cover-preview" />
+              )}
+              {cover && <img src={URL.createObjectURL(cover)} alt="Movie Cover" className="cover-preview" />}
+            </div>
+            <div className="form-group">
+              <label htmlFor="video">Movie File (MP4)</label>
+              <div className="custom-file-input">
+                <input type="file" id="video" accept="video/mp4" onChange={(e) => setVideo(e.target.files[0])} />
+                <span>Choose MP4 File</span>
+              </div>
+              {movieDetails.videoS3Url && !video && (
+                  <video src={movieDetails.videoS3Url} controls className="video-preview"></video>
+              )}
+              {video && <video src={URL.createObjectURL(video)} controls className="video-preview"></video>}
+            </div>
+            <button type="submit" className="submit-button">
+              {isEditMode ? "Update" : "Upload"} Movie
+            </button>
+          </form>
+        </div>
+      </>
   );
 }
 
