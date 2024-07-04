75597f32d60a0a798ce6fc9c12f1f144e1a158d6
Teodor Vidakovic <teodorv22@gmail.com>
2024-07-05 01:17:43 +0200

    feat: create lambda get-downloads fn

---
+++ infra/lib/lambda/src/get-downloads/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { QueryCommand, DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
+
+const dynamoClient = new DynamoDBClient({});
+const dynamoDocClient = DynamoDBDocumentClient.from(dynamoClient);
+
+exports.handler = async (event) => {
+    const tableName = process.env.DOWNLOADS_TABLE;
+    const { Authorization } = event.headers;
+
+    const userId = JSON.parse(Buffer.from(Authorization.split(".")[1], "base64").toString()).sub;
+
+    const dynamoQueryCommand = new QueryCommand({
+        TableName: tableName,
+        KeyConditionExpression: "UserId = :userId",
+        ExpressionAttributeValues: {
+            ":userId": userId,
+        },
+    });
+
+    const downloadsResponse = await dynamoDocClient.send(dynamoQueryCommand);
+    const downloadsList = downloadsResponse.Items.map((download) => download.MovieId);
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods": "POST,OPTIONS",
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(downloadsList),
+    };
+};
