a11b1755972aaee54f5bed490253480350a78f5d
Teodor Vidakovic <teodorv22@gmail.com>
2024-07-05 01:17:33 +0200

    feat: create lambda get-ratings fn

---
+++ infra/lib/lambda/src/get-ratings/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { QueryCommand, DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
+
+const dynamoClient = new DynamoDBClient({});
+const dynamoDocClient = DynamoDBDocumentClient.from(dynamoClient);
+
+exports.handler = async (event) => {
+    const tableName = process.env.RATINGS_TABLE;
+    const { Authorization } = event.headers;
+
+    const userId = JSON.parse(Buffer.from(Authorization.split(".")[1], "base64").toString()).sub;
+
+    const dynamoQueryCommand = new QueryCommand({
+        TableName: tableName,
+        KeyConditionExpression: "UserId = :userId",
+        ExpressionAttributeValues: {
+            ":userId": userId,
+        },
+    });
+
+    const ratingsResponse = await dynamoDocClient.send(dynamoQueryCommand);
+    const ratingsList = ratingsResponse.Items.map((rating) => ({
+        movieId: rating.MovieId,
+        rating: rating.Rating,
+    }));
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods": "POST,OPTIONS",
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(ratingsList),
+    };
+};
