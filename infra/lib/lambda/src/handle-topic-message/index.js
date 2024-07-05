d009d11d5d8fc2088900a1379a8925fdedd0029d
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-07-05 08:43:45 +0200

    feat: Add Unsubscribe method and refactor lambda

--- infra/lib/lambda/src/handle-topic-message/index.js
+++ infra/lib/lambda/src/handle-topic-message/index.js
@@ -3,46 +3,91 @@ const {
   ListTopicsCommand,
   CreateTopicCommand,
   PublishCommand,
-  SubscribeCommand
+  SubscribeCommand,
+  UnsubscribeCommand,
+  ListSubscriptionsByTopicCommand,
 } = require("@aws-sdk/client-sns");
 
 const snsClient = new SNSClient({});
-
 exports.handler = async (event) => {
+
   for (const record of event.Records) {
     const message = JSON.parse(record.body);
-    const {topicName, email} = message;
+    const {topicName, email, unsubscribe} = message;
     const filteredTopicName = topicName.replace(/[^a-zA-Z0-9-_]/g, '-').toLowerCase().trim()
-    // Check if SNS topic exists
-    let topicArn;
-    const listTopicsCommand = new ListTopicsCommand({});
-    const listTopicsResponse = await snsClient.send(listTopicsCommand);
-    const existingTopic = listTopicsResponse.Topics.find((t) => t.TopicArn.includes(filteredTopicName));
-
-    if (existingTopic) {
-      topicArn = existingTopic.TopicArn;
-    } else {
-      const createTopicCommand = new CreateTopicCommand({Name: filteredTopicName});
-      const createTopicResponse = await snsClient.send(createTopicCommand);
-      topicArn = createTopicResponse.TopicArn;
-    }
+    let topicArn = await getTopicArn(filteredTopicName);
 
     if (!email) {
-      const publishCommand = new PublishCommand({
-        TopicArn: topicArn,
-        Message: JSON.stringify(`A new ${topicName} movie has been released.`),
-        Subject: "New Movie Release",
-      });
-
-      await snsClient.send(publishCommand);
+      await publish(topicArn, topicName);
     } else {
-      const subscribeCommand = new SubscribeCommand({
-        Protocol: "email",
-        TopicArn: topicArn,
-        Endpoint: email,
-      });
-
-      await snsClient.send(subscribeCommand);
+      if (unsubscribe) {
+        await unsubscribeFromTopic(topicArn, email);
+      }
+      else
+      {
+        await subscribe(topicArn, email);
+      }
     }
   }
+}
+
+async function subscribe(topicArn, email) {
+  const subscribeCommand = new SubscribeCommand({
+    Protocol: "email",
+    TopicArn: topicArn,
+    Endpoint: email,
+  });
+
+  await snsClient.send(subscribeCommand);
+}
+
+async function getTopicArn(filteredTopicName) {
+  // Check if SNS topic exists
+  const listTopicsCommand = new ListTopicsCommand({});
+  const listTopicsResponse = await snsClient.send(listTopicsCommand);
+  const existingTopic = listTopicsResponse.Topics.find((t) => t.TopicArn.includes(filteredTopicName));
+
+  if (existingTopic) {
+    return existingTopic.TopicArn;
+  }
+  const createTopicCommand = new CreateTopicCommand({Name: filteredTopicName});
+  const createTopicResponse = await snsClient.send(createTopicCommand);
+  return createTopicResponse.TopicArn;
+}
+
+async function publish(topicArn, topicName) {
+  const publishCommand = new PublishCommand({
+    TopicArn: topicArn,
+    Message: JSON.stringify(`A new ${topicName} movie has been released.`),
+    Subject: "New Movie Release",
+  });
+
+  await snsClient.send(publishCommand);
+}
+
+async function unsubscribeFromTopic(topicArn, userEmail) {
+    // List subscriptions for the topic
+    const listSubscriptionsCommand = new ListSubscriptionsByTopicCommand({
+      TopicArn: topicArn
+    });
+    const listSubscriptionsResponse = await snsClient.send(listSubscriptionsCommand);
+
+    // Find the subscription ARN for the userEmail
+    let subscriptionArn;
+    for (const subscription of listSubscriptionsResponse.Subscriptions) {
+      if (subscription.Endpoint === userEmail) {
+        subscriptionArn = subscription.SubscriptionArn;
+        break;
+      }
+    }
+
+    if (!subscriptionArn) {
+      throw new Error(`Subscription not found for email: ${userEmail}`);
+    }
+
+    // Unsubscribe the user
+    const unsubscribeCommand = new UnsubscribeCommand({
+      SubscriptionArn: subscriptionArn
+    });
+    await snsClient.send(unsubscribeCommand);
 }
