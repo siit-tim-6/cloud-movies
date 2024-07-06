19ff0be3b4e57f3b1df243dd2ff5622f215c4d65
Teodor Vidakovic <teodorv22@gmail.com>
2024-07-06 23:39:39 +0200

    feat: implement fetching movies from generate-feed

--- src/components/home/home-movie/home-movie.jsx
+++ src/components/home/home-movie/home-movie.jsx
@@ -1,32 +1,32 @@
 import { Button } from "@/components/ui/button";
 import "./home-movie.css";
-import MovieCover from "@/assets/movie-placeholder.webp";
 import { PlayIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 import { Badge } from "@/components/ui/badge";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function HomeMovie() {
+function HomeMovie({ movie }) {
+  const genres = movie.Genres?.L?.map((genre) => genre.S) || [];
+
   return (
-    <div className="home-movie" style={{ backgroundImage: `url(${MovieCover})` }}>
-      <div className="movie-details">
-        <Badge className="text-lg px-4 py-1 rounded-full font-medium">Sci-Fi</Badge>
-        <h1>Star Wars</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iure minus rerum et qui ipsam, cum error beatae numquam vero voluptates illum accusamus
-          officia officiis! Distinctio nulla necessitatibus veniam fuga.
-        </p>
-        <div className="details-buttons">
-          <Button variant="outline" className="text-lg py-8 px-10 rounded-full font-medium">
-            <PlayIcon className="mr-2" /> Watch Movie
-          </Button>
-          <Link to="/movies/1">
-            <Button className="text-lg py-8 px-10 rounded-full font-medium bg-transparent border-2 border-slate-400">
-              More Info <ArrowRightIcon className="ml-2" />
+      <div className="home-movie" style={{ backgroundImage: `url(${movie.CoverS3Url.S || MovieCover})` }}>
+        <div className="movie-details">
+          {genres.map((genre, i) => (
+              <Badge key={i} className="text-lg px-4 py-1 rounded-full font-medium">{genre}</Badge>
+          ))}
+          <h1>{movie.Title.S}</h1>
+          <p>{movie.Description.S}</p>
+          <div className="details-buttons">
+            <Button variant="outline" className="text-lg py-8 px-10 rounded-full font-medium">
+              <PlayIcon className="mr-2" /> Watch Movie
             </Button>
-          </Link>
+            <Link to={`/movies/${movie.MovieId.S}`}>
+              <Button className="text-lg py-8 px-10 rounded-full font-medium bg-transparent border-2 border-slate-400">
+                More Info <ArrowRightIcon className="ml-2" />
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
