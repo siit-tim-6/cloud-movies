ca9fc2e81702300f880f5e9dde2bc4f87649d323
Vladimir Popov <vladapopov02@gmail.com>
2024-07-08 13:29:21 +0200

    feat: add transcoded videos bucket, transcoding status table and set event notification to sqs when file is uploaded to movies bucket to data stack

--- infra/lib/data/data-stack.ts
+++ infra/lib/data/data-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3n from "aws-cdk-lib/aws-s3-notifications";
+import * as sqs from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
 
 export class DataStack extends cdk.Stack {
@@ -8,6 +10,9 @@ export class DataStack extends cdk.Stack {
   public readonly subscriptionsDataTable: dynamodb.Table;
   public readonly moviesBucket: s3.Bucket;
   public readonly movieRatingsTable: dynamodb.Table;
+  public readonly transcodedVideosBucket: s3.Bucket;
+  public readonly transcodingStatusTable: dynamodb.Table;
+  public readonly transcodingQueue: sqs.Queue;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -68,5 +73,30 @@ export class DataStack extends cdk.Stack {
       writeCapacity: 1,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    this.transcodedVideosBucket = new s3.Bucket(this, "TranscodedVideosBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      cors: [
+        {
+          allowedOrigins: ["*"],
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT, s3.HttpMethods.POST, s3.HttpMethods.DELETE],
+          allowedHeaders: ["*"],
+        },
+      ],
+    });
+
+    this.transcodingStatusTable = new dynamodb.Table(this, "TrancodingStatus", {
+      partitionKey: { name: "MovieId", type: dynamodb.AttributeType.STRING },
+      readCapacity: 1,
+      writeCapacity: 1,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    this.transcodingQueue = new sqs.Queue(this, "transcodingQueue", {
+      visibilityTimeout: cdk.Duration.hours(1),
+    });
+
+    this.moviesBucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT, new s3n.SqsDestination(this.transcodingQueue));
   }
 }
