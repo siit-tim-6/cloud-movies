33ef402dd6139874847a036fa37a23e884f0c2b4
Vladimir Popov <vladapopov02@gmail.com>
2024-07-08 13:33:18 +0200

    feat: add video transcoding stack and define dependencies

--- infra/bin/cloud-movies-be.ts
+++ infra/bin/cloud-movies-be.ts
@@ -6,7 +6,8 @@ import { ApiGwStack } from "../lib/api-gw/api-gw-stack";
 import { FrontDeploymentStack } from "../lib/front-deployment/front-deployment-stack";
 import { DataStack } from "../lib/data/data-stack";
 import { LambdaStack } from "../lib/lambda/lambda-stack";
-import {SqsStack} from "../lib/sqs/sqs-stack";
+import { SqsStack } from "../lib/sqs/sqs-stack";
+import { VideoTranscodingStack } from "../lib/video-transcoding/video-transcoding-stack";
 
 const app = new cdk.App();
 
@@ -39,5 +40,12 @@ new ApiGwStack(app, "ApiGwStack", {
 });
 
 new FrontDeploymentStack(app, "FrontDeploymentStack", {});
+
+new VideoTranscodingStack(app, "VideoTranscodingStack", {
+  moviesBucket: dataStack.moviesBucket,
+  transcodedVideosBucket: dataStack.transcodedVideosBucket,
+  transcodingStatusTable: dataStack.transcodingStatusTable,
+  transcodingQueue: dataStack.transcodingQueue,
+});
 
 app.synth();
