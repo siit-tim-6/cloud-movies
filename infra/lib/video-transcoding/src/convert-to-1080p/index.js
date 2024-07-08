c23143bfdff93e883d0ee1aca13a2fbb69f6e558
Vladimir Popov <vladapopov02@gmail.com>
2024-07-08 13:30:06 +0200

    feat: implement conversion lambdas

--- infra/lib/video-transcoding/src/convert-to-1080p/index.js
+++ infra/lib/video-transcoding/src/convert-to-1080p/index.js
@@ -1,73 +1,85 @@
-const S3 = require("aws-sdk/clients/s3");
+"use strict";
+
+const { GetObjectCommand, S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
 const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
+const fsPromises = fs.promises;
 
-const s3 = new S3({
-  region: "eu-central-1",
-});
+const s3Client = new S3Client();
 
-const process1080p = (event) => {
+const process1080p = async (event) => {
   const videoKey = event.key;
   const inputBucketName = process.env.S3_INPUT_BUCKET;
   const outputBucketName = process.env.S3_OUTPUT_BUCKET;
   const id = videoKey.split("/")[0];
   const filename = videoKey.split("/")[3];
   const params = { Bucket: inputBucketName, Key: videoKey };
-  var totalTime;
   const downloadFile = `/tmp/${id}/${filename}`;
 
-  fs.mkdirSync(`/tmp/${id}`, { recursive: true });
+  await fsPromises.mkdir(`/tmp/${id}`, { recursive: true });
 
   const file = fs.createWriteStream(downloadFile);
-  s3.getObject(params).createReadStream().pipe(file);
+  const s3Response = await s3Client.send(new GetObjectCommand(params));
+  s3Response.Body.pipe(file);
 
-  ffmpeg(downloadFile)
-    .on("start", () => {
-      console.log(`transcoding ${id} to 1080p`);
-    })
-    .on("error", (err, stdout, stderr) => {
-      console.log("stderr:", stderr);
-      console.error(err);
-    })
-    .on("end", async () => {
-      const fileUploadPromises = fs.readdirSync(`/tmp/${id}`).map((file) => {
-        let params = { Bucket: outputBucketName, Key: `${id}/${file}`, Body: fs.readFileSync(`/tmp/${id}/${file}`) };
-        console.log(`uploading ${file} to s3`);
-        return s3.putObject(params).promise();
-      });
-      await Promise.all(fileUploadPromises);
-      await fs.rmdirSync(`/tmp/${id}`, { recursive: true });
-      console.log(`tmp is deleted!`);
-    })
-    .on("codecData", (data) => {
-      totalTime = parseInt(data.duration.replace(/:/g, ""));
-    })
-    .on("progress", (progress) => {
-      const time = parseInt(progress.timemark.replace(/:/g, ""));
-      const percent = Math.ceil((time / totalTime) * 100);
-      console.log(`progress :- ${percent}%`);
-    })
-    .outputOptions([
-      "-vf scale=w=1920:h=1080",
-      "-c:a aac",
-      "-ar 48000",
-      "-b:a 192k",
-      "-c:v h264",
-      "-profile:v main",
-      "-crf 20",
-      "-g 48",
-      "-keyint_min 48",
-      "-sc_threshold 0",
-      "-b:v 5000k",
-      "-maxrate 5350k",
-      "-bufsize 7500k",
-      "-f hls",
-      "-hls_time 4",
-      "-hls_playlist_type vod",
-      `-hls_segment_filename /tmp/${id}/1080p_%d.ts`,
-    ])
-    .output(`/tmp/${id}/1080p.m3u8`)
-    .run();
+  await transcode(downloadFile, id, outputBucketName);
+};
+
+const transcode = async (downloadFile, id, outputBucketName) => {
+  let totalTime;
+
+  return new Promise((resolve, reject) => {
+    ffmpeg(downloadFile)
+      .on("start", () => {
+        console.log(`transcoding ${id} to 1080p`);
+      })
+      .on("error", (err, stdout, stderr) => {
+        console.log("stderr:", stderr);
+        console.error(err);
+        reject(new Error(err));
+      })
+      .on("end", async () => {
+        const fileUploadPromises = (await fsPromises.readdir(`/tmp/${id}`)).map((file) => {
+          let params = { Bucket: outputBucketName, Key: `${id}/${file}`, Body: fs.readFileSync(`/tmp/${id}/${file}`) };
+          console.log(`uploading ${file} to s3`);
+          return s3Client.send(new PutObjectCommand(params));
+        });
+        await Promise.all(fileUploadPromises);
+        await fsPromises.rm(`/tmp/${id}`, { recursive: true });
+        console.log(`tmp is deleted!`);
+        resolve();
+      })
+      .on("codecData", (data) => {
+        totalTime = parseInt(data.duration.replace(/:/g, ""));
+      })
+      .on("progress", (progress) => {
+        const time = parseInt(progress.timemark.replace(/:/g, ""));
+        const percent = Math.ceil((time / totalTime) * 100);
+        console.log(`progress :- ${percent}%`);
+      })
+      .outputOptions([
+        "-vf scale=w=1920:h=1080",
+        "-c:a aac",
+        "-ar 48000",
+        "-b:a 192k",
+        "-c:v h264",
+        "-profile:v main",
+        "-crf 20",
+        "-g 48",
+        "-keyint_min 48",
+        "-sc_threshold 0",
+        "-b:v 5000k",
+        "-maxrate 5350k",
+        "-bufsize 7500k",
+        "-f hls",
+        "-hls_time 4",
+        "-hls_playlist_type vod",
+        `-hls_segment_filename /tmp/${id}/1080p_%d.ts`,
+      ])
+      .output(`/tmp/${id}/1080p.m3u8`)
+      .run();
+  });
 };
 
 module.exports = {
