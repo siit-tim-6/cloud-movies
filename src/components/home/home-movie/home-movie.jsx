452c479df42c82288210775f9a6e2072dca679a4
Vladimir Popov <vladapopov02@gmail.com>
2024-07-09 04:59:36 +0200

    feat: apply styliing to home movie genre badge

--- src/components/home/home-movie/home-movie.jsx
+++ src/components/home/home-movie/home-movie.jsx
@@ -8,22 +8,24 @@ function HomeMovie({ movie }) {
   const genres = movie.Genres || [];
 
   return (
-      <div className="home-movie" style={{ backgroundImage: `url(${movie.CoverS3Url})` }}>
-        <div className="movie-details">
-          {genres.map((genre, i) => (
-              <Badge key={i} className="text-lg px-4 py-1 rounded-full font-medium">{genre}</Badge>
-          ))}
-          <h1>{movie.Title}</h1>
-          <p>{movie.Description}</p>
-          <div className="details-buttons">
-              <Link to={`/movies/${movie.MovieId}`}>
-                  <Button className="text-lg py-8 px-10 rounded-full font-medium" style={{ backgroundColor: 'white', color: 'black' }}>
-                      Watch <ArrowRightIcon className="ml-2"/>
-                  </Button>
-              </Link>
-          </div>
+    <div className="home-movie" style={{ backgroundImage: `url(${movie.CoverS3Url})` }}>
+      <div className="movie-details">
+        {genres.map((genre, i) => (
+          <Badge key={i} className="text-lg px-4 py-1 rounded-full font-medium uppercase mr-3">
+            {genre}
+          </Badge>
+        ))}
+        <h1>{movie.Title}</h1>
+        <p>{movie.Description}</p>
+        <div className="details-buttons">
+          <Link to={`/movies/${movie.MovieId}`}>
+            <Button className="text-lg py-8 px-10 rounded-full font-medium" style={{ backgroundColor: "white", color: "black" }}>
+              Watch <ArrowRightIcon className="ml-2" />
+            </Button>
+          </Link>
         </div>
       </div>
+    </div>
   );
 }
 
